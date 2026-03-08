afa94a451ae208b1ed139e8061eec2785dd8eef8
AlexandruMunteanu89 <sasamunteanu18@gmail.com>
2026-03-08 20:27:10 +0100

    Creiamo una funzione per invertire la parola

--- js-palidroma/script.js
+++ js-palidroma/script.js
@@ -2,9 +2,8 @@
 const parola = prompt("inserisci una parola");
 console.log(parola);
 
-let parolaInvertita = '';
-
 // Creiamo una funzione per invertire la parola
+let parolaInvertita = '';
 function invertiParola(str) {
     
     for (let i = str.length - 1; i >= 0; i--){
@@ -14,11 +13,5 @@ function invertiParola(str) {
 }
 console.log(invertiParola(parola));
 
-// Controlliamo se la parola è uguale a la parola invertita
-if (parola === parolaInvertita){
-    console.log("parola è palindroma")
-} else {
-    console.log("parola non è palindroma");
-    
-}
+
 
